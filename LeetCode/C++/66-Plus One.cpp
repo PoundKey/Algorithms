cf81b2dabf4469de51d2a3f5d79071218976a2a5
C.T Xue <xuecnt@hotmail.com>
2015-10-18 02:00:34 -0700

    Recursion Practice

---
+++ LeetCode/C++/66-Plus One.cpp
@@ -0,0 +1,54 @@
+// Thinking: Trace to the end of the vector, and return 1 if vector[last] + 1 >= 10
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int result = trace(digits, 0);
+        if (result) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+    
+    int trace(vector<int>& digits, int index) {
+        if (index == digits.size()-1) {
+            return addUp(digits, index);
+        }
+        int result = trace(digits, index+1);
+        if (result == 1) {
+            return addUp(digits, index);
+        } 
+        return 0;
+    }
+    
+    int addUp(vector<int>& digits, int index) {
+            int res = digits[index] += 1;
+            digits[index] = res % 10;
+            return (res >= 10) ? 1 : 0;
+    }
+};
+
+
+// Thinking: Covert back to integer and plus one, then convert the integer back to a vector
+// Did not solve the INT overflow problem
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int sum = 0;
+        for (int i=0; i<digits.size(); i++) {
+            sum = sum*10 + digits[i];
+        }
+        sum += 1;
+        vector<int> result;
+        generateVec(sum, result);
+        return result;
+    }
+    
+    void generateVec(int num, vector<int> &vec) {
+        if (num < 10) {
+            vec.push_back(num);
+            return;
+        }
+        generateVec(num/10, vec);
+        vec.push_back(num % 10);
+    }
+};
--- LeetCode/C++/80-Remove Duplicates from Sorted Array II.cpp
+++ LeetCode/C++/80-Remove Duplicates from Sorted Array II.cpp
@@ -6,7 +6,7 @@ public:
         int j = 0, count = 0;
         for (int i=1; i < nums.size(); i++) {
             if (nums[i] == nums[j]) {
-            	count++;
+            		count++;
                 if (count < 2) {
                     nums[++j] = nums[i];
                 }
