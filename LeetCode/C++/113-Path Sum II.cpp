6701f7b991cffafb5059a234a4f96ec33b4296a8
C.T Xue <xuecnt@hotmail.com>
2015-10-25 14:14:06 -0700

    LinkedList Started

--- LeetCode/C++/113-Path Sum II.cpp
+++ LeetCode/C++/113-Path Sum II.cpp
@@ -10,6 +10,21 @@
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        
+        vector< vector<int> > res;
+        vector<int> path;
+        DFS(res, path, sum, root);
+        return res;
+    }
+    
+    void DFS(vector< vector<int> >& res, vector<int> path, int sum, TreeNode* root) {
+        if (root == NULL) return;
+        int val = root->val;
+        path.push_back(val);
+        if (val == sum && root->left == NULL && root->right == NULL) {
+            res.push_back(path);
+            return;
+        }
+        DFS(res, path, sum-val, root->left);
+        DFS(res, path, sum-val, root->right);
     }
 };
--- LeetCode/C++/2-Add Two Numbers.cpp
+++ LeetCode/C++/2-Add Two Numbers.cpp
@@ -1,2 +1,35 @@
-// Thoughts: 
-
+// Thoughts: Situations to notice
+// 1: if any of the linklist is null, e.g., L1: NULL, L2: 1->2  return: 1->2
+// 2: if the final linklist is longer than any of the two:
+// L1: 2->2, L2: 9->9, return: 1->2->1
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dnode = new ListNode(0);
+        ListNode* p = dnode;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            if (l1) {
+                carry += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                carry += l2->val;
+                l2 = l2->next;
+            }
+            ListNode* node = new ListNode(carry % 10);
+            carry = carry / 10;
+            p->next = node;
+            p = p->next;
+        }
+        return dnode->next;
+    }
+};
