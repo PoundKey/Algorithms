b5a5752f6c03ca33cdd345c9751ff82fd83503b9
CX <ubccs@alumni.ubc.ca>
2018-01-21 04:54:57 -0800

    Create 35. Reverse Linked List.cpp

---
+++ LintCode/35. Reverse Linked List.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    ListNode *reverse(ListNode *head) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode* prev = NULL;
+        while (head != NULL) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+};
