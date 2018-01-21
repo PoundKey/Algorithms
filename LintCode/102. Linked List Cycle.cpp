0a3b461697c998fe83ef9dcb762236cc11f13f40
CX <ubccs@alumni.ubc.ca>
2018-01-21 04:54:17 -0800

    Create 102. Linked List Cycle.cpp

---
+++ LintCode/102. Linked List Cycle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    bool hasCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL) return false;
+        ListNode* slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+};
