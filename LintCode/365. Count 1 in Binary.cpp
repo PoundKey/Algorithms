eb5030ca7fbc290f6efbbfca0fd8d06fa287434e
CX <ubccs@alumni.ubc.ca>
2018-01-21 04:06:41 -0800

    Create 365. Count 1 in Binary.cpp

---
+++ LintCode/365. Count 1 in Binary.cpp
@@ -0,0 +1,17 @@
+// n & (n - 1) == remove the right-most 1 bit of n
+class Solution {
+public:
+    /**
+     * @param num: an integer
+     * @return: an integer, the number of ones in num
+     */
+    int countOnes(int num) {
+        // write your code here
+        int count = 0;
+        while (num != 0) {
+            num = num & (num - 1);
+            count++;
+        }
+        return count;
+    }
+};
