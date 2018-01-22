762c5289a8d56a5c8dc3624e8632d601aef05bc7
CX <ubccs@alumni.ubc.ca>
2018-01-21 20:26:53 -0800

    Create 114. Unique Paths.cpp

---
+++ LintCode/114. Unique Paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /**
+     * @param n, m: positive integer (1 <= n ,m <= 100)
+     * @return an integer
+     */
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
