7ea40945d95f2bb5449be8b38a8cb29805adc999
CX <ubccs@alumni.ubc.ca>
2018-01-21 21:48:34 -0800

    Create 517. Ugly Number.cpp

---
+++ LintCode/517. Ugly Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    /**
+     * @param num an integer
+     * @return true if num is an ugly number or false
+     */
+    bool isUgly(int num) {
+        if (num <= 0) return false;
+        if (num == 1) return true;
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
+    }
+};
