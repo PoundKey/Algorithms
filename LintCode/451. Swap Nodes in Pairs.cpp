58d75f306e16b8335bb1c73c4507add019f6aa99
CX <ubccs@alumni.ubc.ca>
2018-02-04 23:32:43 -0800

    Update 451. Swap Nodes in Pairs.cpp

--- LintCode/451. Swap Nodes in Pairs.cpp
+++ LintCode/451. Swap Nodes in Pairs.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
-    /**
-     * @param head a ListNode
-     * @return a ListNode
-     */
     ListNode* swapPairs(ListNode* head) {
         if (head == NULL || head->next == NULL) return head;
-        
         ListNode* next = head->next, *nextNext = next->next;
         next->next = head;
         head->next = swapPairs(nextNext);
         return next;
     }
+    
+    ListNode* swapPairsIter(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode dummy(0, head);
+        ListNode* prev = &dummy;
+        
+        while (head && head->next)
+        {
+            ListNode* next = head->next;
+            ListNode* nextNext = next->next;
+            next->next = head;
+            prev->next = next;
+            prev = head;
+            head->next = nextNext;
+            head = nextNext;
+        }
+        return dummy.next;
+    }    
 };
