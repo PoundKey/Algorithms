4cc38ae9d08f7fa66e6ec0e62e158c140d1a2c5f
CX <ubccs@alumni.ubc.ca>
2018-03-19 23:44:35 -0700

    Update 67. Binary Tree Inorder Traversal.cpp

--- LintCode/67. Binary Tree Inorder Traversal.cpp
+++ LintCode/67. Binary Tree Inorder Traversal.cpp
@@ -17,4 +17,29 @@ public:
         res.push_back(root->val);
         traverse(root->right, res);
     }
+    
+    // Iteratively
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> res(0);
+        if (root == NULL) return res;
+        TreeNode* node = root;
+        stack<TreeNode*> s;
+        while (!s.empty() || node)
+        {
+            if (node)
+            {
+                s.push(node);
+                node = node->left;
+            }
+            else
+            {
+                node = s.top();
+                res.push_back(node->val);
+                s.pop();
+                node = node->right;
+            }
+        }
+        return res;
+    }    
+    
 };
