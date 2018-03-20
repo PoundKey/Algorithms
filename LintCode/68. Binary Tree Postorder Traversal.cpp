5d17a38a2b7209edb504721ae4a5041b4f25dcae
CX <ubccs@alumni.ubc.ca>
2018-03-19 23:55:16 -0700

    Update 68. Binary Tree Postorder Traversal.cpp

--- LintCode/68. Binary Tree Postorder Traversal.cpp
+++ LintCode/68. Binary Tree Postorder Traversal.cpp
@@ -15,4 +15,26 @@ public:
         DFS(root->right, res);
         res.push_back(root->val);
     }
+    // Iteratively, two stacks approach
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> res;
+        if (root == NULL) return res;
+        stack<TreeNode*> s1, s2;
+        s1.push(root);
+        while (!s1.empty())
+        {
+            TreeNode* node = s1.top();
+            s1.pop();
+            s2.push(node);
+            if (node->left) s1.push(node->left);
+            if (node->right) s1.push(node->right);
+        }
+        while (!s2.empty())
+        {
+            TreeNode* node = s2.top();
+            s2.pop();
+            res.push_back(node->val);
+        }
+        return res;
+    } 
 };
