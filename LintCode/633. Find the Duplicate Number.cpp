1d17a9ab60c306c38e152fce51251f07f8bc6c68
CX <ubccs@alumni.ubc.ca>
2018-03-26 03:31:29 -0700

    Create 633. Find the Duplicate Number.cpp

---
+++ LintCode/633. Find the Duplicate Number.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    /**
+     * @param nums: an array containing n + 1 integers which is between 1 and n
+     * @return: the duplicate one
+     */
+     
+    int findDuplicate(vector<int> &nums) {
+        if (nums.empty()) return -1;
+        int n = nums.size();
+        int start = 0, end = n - 1;
+        
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            int counter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] <= mid)
+                {
+                    counter++;
+                }
+            }
+            if (counter > mid)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    
+    int findDuplicateAlt(vector<int> &nums) {
+        if (nums.empty()) return -1;
+        set<int> set;
+        for (const auto& it : nums)
+        {
+            if (set.count(it)) return it;
+            set.insert(it);
+        }
+        return -1;
+    }
+};
