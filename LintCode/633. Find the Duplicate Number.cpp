7f565645a2f0628f614742675121396d1e051518
CX <ubccs@alumni.ubc.ca>
2018-03-26 03:38:21 -0700

    Update 633. Find the Duplicate Number.cpp

--- LintCode/633. Find the Duplicate Number.cpp
+++ LintCode/633. Find the Duplicate Number.cpp
@@ -8,7 +8,7 @@ public:
     int findDuplicate(vector<int> &nums) {
         if (nums.empty()) return -1;
         int n = nums.size();
-        int start = 0, end = n - 1;
+        int start = 1, end = n - 1;
         
         while (start <= end)
         {
