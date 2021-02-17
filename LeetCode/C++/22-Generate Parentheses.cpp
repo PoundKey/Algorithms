f404744dc4aea536675726eab0a68f8507a12f4a
CX <ubccs@alumni.ubc.ca>
2021-02-17 16:44:10 -0500

    Update 22-Generate Parentheses.cpp

--- LeetCode/C++/22-Generate Parentheses.cpp
+++ LeetCode/C++/22-Generate Parentheses.cpp
@@ -1,31 +1,35 @@
-// Thoughts: Typical backtracking problem, we need to think a bit more about the constraint.
-// 1. we are allowed to push back '(' as long as it's less than n
-// 2. to be well-formed, we push ')' only if '(' is more than ')'
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        if (n == 0) return res;
-        string sol;
-        DFS(res, sol, n, 0, 0);
+        if (0 == n)
+        {
+            return res;
+        }
+        
+        backtrack(res, "", n, 0, 0);
         return res;
     }
-    void DFS(vector<string>& res, string& sol, int n, int left, int right) {
-        if (left == n && right == n) {
-            res.push_back(sol);
+    
+    /**
+        本质：pre-order DFS
+    **/
+    void backtrack(vector<string>& res, string curr, int n, int left, int right)
+    {
+        if (curr.size() == 2 * n)
+        {
+            res.push_back(curr);
             return;
         }
         
-        if (left < n) {
-            sol.push_back('(');
-            DFS(res, sol, n, left + 1, right);
-            sol.pop_back();
+        if (left < n)
+        {
+            backtrack(res, curr + "(", n, left + 1, right);
         }
         
-        if (right < left) {
-            sol.push_back(')');
-            DFS(res, sol, n, left, right + 1);
-            sol.pop_back();
+        if (right < left)
+        {
+            backtrack(res, curr + ")", n, left, right + 1);    
         }
     }
 };
