ad0a12f52e799ad6e8c57775b6d3f08a7a6058b6
CX <ubccs@alumni.ubc.ca>
2021-02-21 01:50:23 -0500

    Update 140-Word Break II.cpp

--- LeetCode/C++/140-Word Break II.cpp
+++ LeetCode/C++/140-Word Break II.cpp
@@ -1,31 +1,101 @@
-// Thoughts: Backtracking without dynamic programming, timeout.
 class Solution {
+private:
+    unordered_map<string, vector<string>> d_memo;    
 public:
-    vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
-        vector<string> res;
-        if (s.empty()) return res;
-        vector<string> sol;
-        DFS(res, sol, wordDict, s, 0);
-        return res;
+    // memorization: top -> down
+    // 1. check memo[T]
+    // 2. calculate memo[T]
+    // 3. set memo[T]
+    // 4. return memo[T]
+    vector<string> wordBreak(string s, vector<string>& wordDict) 
+    {
+        if (s.empty() || wordDict.empty())
+        {
+            return {};
+        }
+        
+        unordered_set<string> dict;
+        for (const auto& str : wordDict)
+        {
+            dict.insert(str);
+        }
+        
+        return wordBreak(s, dict);
     }
     
-    void DFS(vector<string>& res, vector<string>& sol, unordered_set<string>& wordDict, string& s, int start) {
-        if (start == s.size() && !sol.empty()) {
-            string builder = sol[0];
-            for (int i = 1; i < sol.size(); i++) {
-                builder += " " + sol[i];
-            }
-            res.push_back(builder);
-            return;
+    vector<string> wordBreak(string s, unordered_set<string>& dict) 
+    {
+        if (d_memo.count(s))
+        {
+            return d_memo[s];
         }
-        for (int i = start; i < s.size(); i++) {
-            string sub = s.substr(start, i - start + 1);
-            if (wordDict.count(sub)) {
-                sol.push_back(sub);
-                DFS(res, sol, wordDict, s, i + 1);
-                sol.pop_back();
+        
+        vector<string> result; // result here
+        if (dict.count(s))
+        {
+            result.push_back(s);
+        }
+        
+        for (int i = 1; i < s.size(); i++)
+        {
+            string strSuffix = s.substr(i);
+            if (dict.count(strSuffix))
+            {
+                string strPrefix = s.substr(0, i);
+                vector<string> subResult = wordBreak(strPrefix, dict);
+                for (const auto& it : subResult)
+                {
+                    result.push_back(it + " " + strSuffix);
+                }
             }
         }
         
+        d_memo[s] = result;
+        return result;
+    }
+    
+    vector<string> wordBreakAlt(string s, vector<string>& wordDict) {
+        vector<string> res;
+        vector<string> curr;
+        
+        unordered_set<string> dict;
+        for (const auto& str : wordDict)
+        {
+            dict.insert(str);
+        }
+        
+        wordBreakBacktrack(res, curr, dict, s);
+        return res;
+    }
+    
+    void wordBreakBacktrack(vector<string>& res, vector<string>& curr, unordered_set<string>& dict, string s)
+    {
+        if (s.empty() && curr.size())
+        {
+            res.push_back(convertToString(curr));
+            return;
+        }
+        
+        for (int i = 1; i <= s.size(); i++)
+        {
+            string prefix = s.substr(0, i);
+            if (dict.count(prefix))
+            {
+                curr.push_back(prefix);
+                wordBreakBacktrack(res, curr, dict, s.substr(i));
+                curr.pop_back();
+            }
+        }
+    }
+    
+    string convertToString(const vector<string>& input)
+    {
+        string res;
+        for (const auto& str : input)
+        {
+            res += str + " ";
+        }
+        res.pop_back();
+        return res;
     }
 };
